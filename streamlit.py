27ac465a9ae73229b5a08a2f0cc252f77f38c1a4
Artemidividiy <artemiy.kasyanik@gmail.com>
2022-06-12 23:44:56 +0300

    maybe now

--- streamlit.py
+++ streamlit.py
@@ -2,20 +2,7 @@ import pandas as pd
 import streamlit as st
 import streamlit_folium as st_folium
 import folium
-import numpy as np
-import pandas as pd
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import StandardScaler, RobustScaler
-from sklearn.pipeline import make_pipeline,Pipeline
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-
-from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_score, cross_val_predict, validation_curve
-from sklearn import metrics
-from sklearn.metrics import make_scorer,mean_squared_error, r2_score
 data_adress = "./data_cleaned.csv"
 
 @st.cache
